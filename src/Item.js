e68ecc1960db3feb2df14ff7733b3de728ca83be
Koki Moribe <koki.moribe+coding@gmail.com>
2017-10-25 19:42:11 -0700

    add Item component

---
+++ src/Item.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+import { Header, Segment, Label } from "semantic-ui-react";
+
+import { Draggable } from "react-beautiful-dnd";
+
+import * as C from "./Constants";
+
+const grid = 8;
+
+const getItemStyle = (draggableStyle, isDragging) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  // background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+const statusToColor = {
+  [C.TO_DO]: "blue",
+  [C.IN_PROGRESS]: "yellow",
+  [C.DONE]: "green"
+};
+
+const Item = props => {
+  const { provided, snapshot, item } = props;
+
+  return (
+    <div>
+      <div
+        ref={provided.innerRef}
+        style={getItemStyle(provided.draggableStyle, snapshot.isDragging)}
+        {...provided.dragHandleProps}
+      >
+        <Segment
+          raised={snapshot.isDragging}
+          disabled={item.isPending}
+          color={statusToColor[item.status]}
+        >
+          {/* <Header as="h3" floated="right">
+            <Label color={statusToColor[item.status]}>{item.status}</Label>
+          </Header> */}
+          <Header as="h3" floated="left">
+            {item.title}
+          </Header>
+          <div style={{ clear: "both" }}>{item.description}</div>
+        </Segment>
+      </div>
+      {provided.placeholder}
+    </div>
+  );
+};
+
+const DraggableItem = props => {
+  const { item } = props;
+
+  return (
+    <Draggable draggableId={item.id}>
+      {(provided, snapshot) => (
+        <Item provided={provided} snapshot={snapshot} {...props} />
+      )}
+    </Draggable>
+  );
+};
+
+export default DraggableItem;
