31422f4ea5f14ac3b28532f5aba4ee270a5cd6a5
Koki Moribe <koki.moribe+coding@gmail.com>
2017-10-27 19:43:42 -0700

    add CreateTaskModal to Board component

--- src/Board.js
+++ src/Board.js
@@ -1,41 +1,36 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Button, Grid, Header } from "semantic-ui-react";
+import { Grid, Header } from "semantic-ui-react";
 
 import Lane from "./Lane";
+import CreateTaskModal from "./CreateTaskModal";
 
-const Board = props => {
-  const { laneIds, lanes, itemsById } = props;
-
+const Board = ({ laneIds, lanes, itemsById }) => {
   return (
-    <Grid stackable>
-      <Grid.Row columns={2}>
-        <Grid.Column floated="left">
-          <Header as="h1">Board</Header>
-        </Grid.Column>
-        <Grid.Column floated="right">
-          <Button
-            primary
-            floated="right"
-            content={"Add a task"}
-            icon="plus"
-            labelPosition="right"
-          />
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row centered columns={laneIds.length}>
-        {laneIds.map(laneId => {
-          const itemIds = lanes[laneId];
-          const items = itemIds.map(id => itemsById[id]);
+    <div>
+      <Grid stackable>
+        <Grid.Row columns={2}>
+          <Grid.Column floated="left">
+            <Header as="h1">Board</Header>
+          </Grid.Column>
+          <Grid.Column floated="right" textAlign="right">
+            <CreateTaskModal />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered columns={laneIds.length}>
+          {laneIds.map(laneId => {
+            const itemIds = lanes[laneId];
+            const items = itemIds.map(id => itemsById[id]);
 
-          return (
-            <Grid.Column key={laneId}>
-              <Lane laneId={laneId} items={items} />
-            </Grid.Column>
-          );
-        })}
-      </Grid.Row>
-    </Grid>
+            return (
+              <Grid.Column key={laneId}>
+                <Lane laneId={laneId} items={items} />
+              </Grid.Column>
+            );
+          })}
+        </Grid.Row>
+      </Grid>
+    </div>
   );
 };
 
