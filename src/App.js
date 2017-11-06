5c42203fe528aafdedec8705657c1d3b7e4fd43f
Koki Moribe <koki.moribe+coding@gmail.com>
2017-11-05 20:53:47 -0800

    add `/callback` and `/home` routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import Header from "./Header";
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
+import Auth from "./Auth/Auth";
 import Home from "./Home.js";
+import Callback from "./Callback/Callback.js";
+
+const auth = new Auth();
+
+const handleAuthentication = (nextState, replace) => {
+  if (/access_token|id_token|error/.test(nextState.location.hash)) {
+    auth.handleAuthentication();
+  }
+};
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header auth={auth} />
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" render={() => <Redirect to="/home" />} />
+          <Route
+            exact
+            path="/home"
+            render={props => <Home auth={auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => {
+              handleAuthentication(props);
+              return <Callback {...props} />;
+            }}
+          />
         </Switch>
       </div>
     );
