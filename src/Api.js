b97077f6d81365093bf1922e86f43ed52621384e
Koki Moribe <koki.moribe+coding@gmail.com>
2017-11-05 21:35:56 -0800

    update Api functions to handle board-related operations

--- src/Api.js
+++ src/Api.js
@@ -3,15 +3,15 @@ import Swagger from "swagger-client";
 const host = "https://todo-api-rd7qf9.herokuapp.com/v1";
 const spec = host + "/swagger.json";
 
-const clientPromise = Swagger(spec);
-
-const sendRequest = (tag, operationId, params) => {
-  return clientPromise.then(client => client.apis[tag][operationId](params));
+const sendRequest = (tag, operationId, params, securities = []) => {
+  return Swagger(spec, {
+    authorizations: { jwt: `Bearer ${localStorage.getItem("access_token")}` }
+  }).then(client => client.apis[tag][operationId](params, securities));
 };
 
-export const getTasks = () => {
-  const tag = "tasks";
-  const operationId = "todo_api_get_tasks";
+export const getBoards = () => {
+  const tag = "boards";
+  const operationId = "todo_api_get_boards";
   const params = {};
 
   return sendRequest(tag, operationId, params)
@@ -19,10 +19,34 @@ export const getTasks = () => {
     .catch(e => console.log(e));
 };
 
-export const createTask = ({ title, description }) => {
+export const getBoard = id => {
+  const tag = "boards";
+  const operationId = "todo_api_get_board";
+  const params = { board_id: id };
+
+  return sendRequest(tag, operationId, params)
+    .then(r => r.body)
+    .catch(e => console.log(e));
+};
+
+export const getDemoBoard = id => {
+  const tag = "boards";
+  const operationId = "todo_api_get_demo_board";
+  const params = {};
+
+  return sendRequest(tag, operationId, params)
+    .then(r => r.body)
+    .catch(e => console.log(e));
+};
+
+export const getTasks = boardId => {
+  return getBoard(boardId).then(board => board.tasks);
+};
+
+export const createTask = (boardId, title, description) => {
   const tag = "tasks";
   const operationId = "todo_api_create_task";
-  const params = { request_body: { title, description } };
+  const params = { board_id: boardId, request_body: { title, description } };
 
   return sendRequest(tag, operationId, params)
     .then(r => r.body)
@@ -33,6 +57,16 @@ export const updateTask = (id, payload) => {
   const tag = "tasks";
   const operationId = "todo_api_update_task";
   const params = { task_id: id, request_body: payload };
+
+  return sendRequest(tag, operationId, params)
+    .then(r => r.body)
+    .catch(e => console.log(e));
+};
+
+export const createBoard = ({ name }) => {
+  const tag = "boards";
+  const operationId = "todo_api_create_board";
+  const params = { request_body: { name } };
 
   return sendRequest(tag, operationId, params)
     .then(r => r.body)
